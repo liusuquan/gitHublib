bda8c14a232e98b08f5587d09e73ac964807d57b
wade_liu <1282686706@qq.com>
2016-08-23 22:09:52 +0800

    add hello.s

---
+++ hello.s
@@ -0,0 +1,32 @@
+	.file	"hello.c"
+	.def	___main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+LC0:
+	.ascii "hello word!!,%d\0"
+	.text
+	.align 2
+.globl _main
+	.def	_main;	.scl	2;	.type	32;	.endef
+_main:
+	pushl	%ebp
+	movl	%esp, %ebp
+	subl	$24, %esp
+	andl	$-16, %esp
+	movl	$0, %eax
+	addl	$15, %eax
+	addl	$15, %eax
+	shrl	$4, %eax
+	sall	$4, %eax
+	movl	%eax, -8(%ebp)
+	movl	-8(%ebp), %eax
+	call	__alloca
+	call	___main
+	movl	$4, -4(%ebp)
+	movl	-4(%ebp), %eax
+	movl	%eax, 4(%esp)
+	movl	$LC0, (%esp)
+	call	_printf
+	movl	$0, %eax
+	leave
+	ret
+	.def	_printf;	.scl	2;	.type	32;	.endef
